0063351a0260546906cce3b79213bbce6d7316d7
Marsya Aurelia Sefira <sefiramarsya@gmail.com>
2025-02-18 13:42:02 +0700

    Jobsheet2_Praktikum2_Langkah7_Modifikasi Single Action Controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return 'Nama : Marsya Aurelia Sefira <br> NIM : 2341720011';
+    }
+}
