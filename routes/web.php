2e791e9d3e0ece50a7d630ff36c9cc17618f5885
Marsya Aurelia Sefira <sefiramarsya@gmail.com>
2025-02-18 14:33:34 +0700

    Jobsheet2_Praktikum3_Langkah6

--- routes/web.php
+++ routes/web.php
@@ -55,6 +55,6 @@ Route::resource('photos', PhotoController::class)->except ([
     'create', 'store', 'update', 'destroy'
 ]);
 
-Route::get('greeting', function () {
-    return view ('hello', ['name' => 'Marsya']);
+Route::get('/greeting', function () {
+    return view ('blog.hello', ['name' => 'Marsya']);
 });
